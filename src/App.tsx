1f457ebd90a912b493ec23803d10401a9037579f
dubeanant <dubeyarjun1071977@gmail.com>
2024-06-24 12:32:41 +0530

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import './index.css'
-import { NavbarDemo } from './components/NavbarMenuDemo'
 /** import { LayoutGridDemo } from './components/LayoutGridDemo' **/
 /** import { StickyScrollRevealDemo } from './components/StickyScrollDemo' **/
 /** import { Wave } from './components/Wave' **/
 /** import { Banner } from './components/Banner' **/
 /** import { TabsDemo } from './components/TabsDemo' **/
 /** import { LoginForm } from './components/LoginForm' **/
-import { TracingBeamDemo } from './components/tracingImplementaion'
+/** import { TracingBeamDemo } from './components/tracingImplementaion' **/
 
 
 function App() {
   return <div >
-    <NavbarDemo />
-    <TracingBeamDemo/>
+
   </div>
 }
 
